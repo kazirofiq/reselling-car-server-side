545c614c7c86926fd79f942a9e7eb5c5dc6b71a3
kazirofiq <rofiqul01941056960@gmail.com>
2022-11-27 21:04:10 +0600

    admin email

--- index.js
+++ index.js
@@ -122,6 +122,23 @@ async function run(){
             const users = await usersCollection.find(query).toArray();
             res.send(users);
         })
+
+        // get admin email user
+
+    // app.get('/users/admin/:email', async(req, res) =>{
+    //     const email = req.params.email;
+    //     const query = {email}
+    //     const user = await usersCollection.findOne(query);
+    //     res.send({isAdmin: user?.role === 'admin'});
+    // })
+
+    app.get('/users/admin/:id', async(req, res) =>{
+        const email = req.params.id;
+        const query = {_id: ObjectId(id)}
+        const user = await usersCollection.findOne(query);
+        res.send({isAdmin: user?.role === 'admin'});
+    })
+
     app.post('/users', async(req, res) =>{
         const user = req.body;
         const result = await usersCollection.insertOne(user);
@@ -130,7 +147,16 @@ async function run(){
     })
 
      // make admin panel
-     app.put('/users/admin/:id',verifyJWT, async(req, res) =>{
+     app.put('/users/admin/:id', verifyJWT,  async(req, res) =>{
+        const decodedEmail = req.decoded.email;
+        const query = {email: decodedEmail};
+        const user = await usersCollection.findOne(query);
+
+        if(user?.role !== 'admin'){
+            return res.status(403).send({message: "forbidden access"})
+
+        }
+
         const id = req.params.id;
         const filter ={_id: ObjectId(id) }
         const options = { upsert: true };
